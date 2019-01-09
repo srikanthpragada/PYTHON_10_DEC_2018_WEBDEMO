acc547fe03b2dbf2a741e19031978f45132e9187
srikanthpragada <srikanthpragada@gmail.com>
2019-01-09 08:29:34 +0530

    09-JAN

--- demo/urls.py
+++ demo/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("listbooks/", book_views.list_books),
     path("addbook/", book_views.add_book),
     path("deletebook/", book_views.delete_book),
+    path('add_cookie/', views.add_cookie ),
+    path('list_cookies/', views.list_cookies),
+    path('session_names/', views.session_names),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 import requests
+import datetime
 
 
 # Create your views here.
@@ -15,7 +17,7 @@ def countries(request):
         return render(request, 'countries.html', {'message': message})
     else:
         countries = resp.json()
-        return render(request, 'countries.html', {'countries' : countries})
+        return render(request, 'countries.html', {'countries': countries})
 
 
 def country_info(request):
@@ -30,7 +32,44 @@ def country_info(request):
             country = resp.json()
 
         return render(request, 'country_info.html',
-                      {'code': code, 'country' : country, 'message' : message})
+                      {'code': code, 'country': country, 'message': message})
     else:
-        return render(request,'country_info.html')
+        return render(request, 'country_info.html')
 
+
+def add_cookie(request):
+    if request.method == "GET":
+        return render(request, 'add_cookie.html')
+    else:  # POST
+        # process data
+        name = request.POST["cookie_name"]
+        value = request.POST["cookie_value"]
+        response = HttpResponseRedirect("/demo/list_cookies/")
+        if 'durable' in request.POST:  # is checkbox selected
+            # Create a durable cookie
+            response.set_cookie(name, value, expires=datetime.datetime.now() + datetime.timedelta(days=10))
+        else:
+            # Create a browser-based cookie
+            response.set_cookie(name, value)
+
+    return response
+
+
+def list_cookies(request):
+    return render(request, 'list_cookies.html', {'cookies': request.COOKIES})
+
+
+def session_names(request):
+    # either take existing names or empty list
+    if 'names' in request.session:
+        names = request.session['names']
+    else:
+        names = []
+
+    if request.method == "POST":
+        # add name to list
+        fullname = request.POST["fullname"]
+        names.append(fullname)
+        request.session['names'] = names
+
+    return render(request, 'session_names.html', {'names': names})
